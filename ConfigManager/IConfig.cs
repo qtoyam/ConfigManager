d7957e809341466a2edb7680dfdabda60f04a2e9
qtoyam <qtoyam@gmail.com>
2022-05-14 00:14:19 +0300

    Add IConfig docs

--- ConfigManager/IConfig.cs
+++ ConfigManager/IConfig.cs
@@ -2,13 +2,39 @@
 {
 	public interface IConfig<T> where T : class, new()
 	{
+		/// <summary>
+		/// Get current value. Not thread-safe.
+		/// </summary>
 		T CurrentValue { get; }
+
+		/// <summary>
+		/// Occurs when <see cref="CurrentValue"/> changed.
+		/// </summary>
 		event ValueChangedHandler<T>? ValueChanged;
 
+		/// <summary>
+		/// Update <see cref="CurrentValue"/>. Thread-safe.
+		/// </summary>
+		/// <param name="applyChanges"></param>
 		void UpdateValue(Action<T> applyChanges);
+
+		/// <summary>
+		/// Asynchronous update <see cref="CurrentValue"/>. Thread-safe.
+		/// </summary>
+		/// <param name="applyChanges"></param>
+		/// <returns></returns>
 		Task UpdateValueAsync(Func<T, Task> applyChanges);
 
+		/// <summary>
+		/// Lock <see cref="CurrentValue"/>, so it will be unchanged untill <see cref="ConfigValueLocker{T}"/> disposed.
+		/// </summary>
+		/// <returns></returns>
 		ConfigValueLocker<T> LockValue();
+
+		/// <summary>
+		/// Asynchronous lock <see cref="CurrentValue"/>, so it will be unchanged untill <see cref="ConfigValueLocker{T}"/> disposed.
+		/// </summary>
+		/// <returns></returns>
 		Task<ConfigValueLocker<T>> LockValueAsync();
 	}
 }
